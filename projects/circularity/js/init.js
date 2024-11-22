837f6844da5f4c528c7e340357dfe39ba8a272e2
miyahjohnson <armiyah.johnson@jpschools.org>
2024-11-22 17:00:52 +0000

    refactor to create 100 circles using loops

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,16 +20,28 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
-        var circle; // variable to hold a single circle when creating circles / iterating
-        var circles = []; // variable to store all circles in one Array
+var circle;
+var circles = []
 
         // TODO 2 : Create a function that draws a circle 
-        drawCircle();
+        function drawCircle() {
+            // Code to draw a circle
+circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
+physikz.addRandomVelocity(circle, canvas);
+view.addChild(circle);
+circles.push(circle);
+        }
 
         // TODO 3 / 7 : Call the drawCircle() function 
-        for (let i = 0; i < 100; i++) {
-            drawCircle();  
-drawCircle();
+        /*drawCircle();
+        drawCircle();
+        drawCircle();
+        drawCircle();
+        drawCircle();*/
+        
+        for (var loopsCompleted = 0; loopsCompleted < 100; loopsCompleted++) {
+            drawCircle();
+        }
 
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
@@ -40,31 +52,28 @@ drawCircle();
         In each frame, for every circle, it should redraw that circle
         and check to see if it has drifted off the screen.         
         */
-        function update(for (let i = 0; i < circles.length; i++) {
-            let circle = circles[i];
-            console.log(`Circle ${i + 1}:`);
-            console.log(`  Position: (${circle.x}, ${circle.y})`);
-            console.log(`  Radius: ${circle.radius}`);
-          }) {
+        function update() {
             // TODO 4 : Update the circle's position //
-            /*physikz.updatePosition(/* Your Bracket Notation HERE );
-            physikz.updatePosition(/* Your Bracket Notation HERE );
-            physikz.updatePosition(/* Your Bracket Notation HERE );
-            physikz.updatePosition(/* Your Bracket Notation HERE );
-            physikz.updatePosition(/* Your Bracket Notation HERE );
-          }
-            
+            /*physikz.updatePosition(circles[0]);
+            physikz.updatePosition(circles[1]);
+            physikz.updatePosition(circles[2]);
+            physikz.updatePosition(circles[3]);
+            physikz.updatePosition(circles[4]);*/
+    
+    
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-            game.checkCirclePosition(/* Your Bracket Notation HERE );
-            game.checkCirclePosition(/* Your Bracket Notation HERE );
-            game.checkCirclePosition(/* Your Bracket Notation HERE );
-            game.checkCirclePosition(/* Your Bracket Notation HERE );
-            game.checkCirclePosition(/* Your Bracket Notation HERE );*/
-            
+           /* game.checkCirclePosition(circles[0]);
+game.checkCirclePosition(circles[1]);
+game.checkCirclePosition(circles[2]);
+game.checkCirclePosition(circles[3]);
+game.checkCirclePosition(circles[4]);*/
+           
 
             // TODO 9 : Iterate over the array
-           
-            
+           for (var i = 0; i < circles.length; i++) {
+            physikz.updatePosition(circles[i])
+            game.checkCirclePosition(circles[i])
+           }
         }
     
         /* 
@@ -80,8 +89,15 @@ drawCircle();
             }
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            
-
+            if (circle.x < 0){
+                circle.x = canvas.width;
+            }
+            if (circle.y < 0){
+                circle.y = canvas.height;
+            }
+            if (circle.y > canvas.height){
+                circle.y = 0;
+            }
 
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
         }
